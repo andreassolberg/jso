3f291b08d2068f98b79c72a718528b39dfd655aa
Andreas <andreas.solberg@uninett.no>
2018-03-19 14:26:58 +0100

    Cleanup popup loader

--- src/Loaders/Popup.js
+++ src/Loaders/Popup.js
@@ -1,37 +1,39 @@
 import BasicLoader from './BasicLoader';
 
 export default class Popup extends BasicLoader {
-	execute() {
-		var that = this;
-		// console.error("Popup loaded...");
 
+  execute() {
+
+		console.error("Popup loaded...");
 		/*
 		* In the popup's scripts, running on <http://example.org>:
 		*/
-		var p = new Promise(function(resolve, reject) {
+		return new Promise((resolve, reject) => {
 
 			window.addEventListener("message", function(event) {
-				console.log("Sent a message to event.origin " + event.origin + " and got the following in response:");
-				console.log("<em>" + event.data + "</em>");
+				console.log("Sent a message to event.origin " + event.origin + " and got the following in response:")
+				console.log("<em>" + event.data + "</em>")
 
-		    	var url = newwindow.location.href;
-		    	console.error("Popup location is ", url, newwindow.location);
-		        resolve(url);
-
+	    	var url = newwindow.location.href
+	    	console.error("Popup location is ", url, newwindow.location)
+        resolve(url)
 			});
-			
-			window.popupCompleted = function() {
-		    	var url = newwindow.location.href;
-		    	console.error("Popup location is ", url, newwindow.location);
-		        resolve(url);
+
+  		window.popupCompleted = function() {
+	    	var url = newwindow.location.href
+	    	console.error("Popup location is ", url, newwindow.location)
+        resolve(url)
 			};
 
-			var newwindow = window.open(that.url, 'uwap-auth', 'height=600,width=800');
+			var newwindow = window.open(this.url, 'uwap-auth', 'height=600,width=800')
+      console.log("Newwindow is ", newwindow)
+      if (newwindow === null) {
+        throw new Error("Error loading popup window")
+      }
 			if (window.focus) {
-				newwindow.focus();
+				newwindow.focus()
 			}
 
-		});
-		return p;
+		})
 	}
 }
