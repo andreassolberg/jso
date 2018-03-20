4508e520bcb03355ef3263ddc3e54fde38abcd6e
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2018-03-20 20:27:49 +0100

    remove logging from popup

--- src/Loaders/Popup.js
+++ src/Loaders/Popup.js
@@ -4,29 +4,29 @@ export default class Popup extends BasicLoader {
 
   execute() {
 
-		console.error("Popup loaded...");
+		// console.error("Popup loaded...");
 		/*
 		* In the popup's scripts, running on <http://example.org>:
 		*/
 		return new Promise((resolve, reject) => {
 
 			window.addEventListener("message", function(event) {
-				console.log("Sent a message to event.origin " + event.origin + " and got the following in response:")
-				console.log("<em>" + event.data + "</em>")
+				// console.log("Sent a message to event.origin " + event.origin + " and got the following in response:")
+				// console.log("<em>" + event.data + "</em>")
 
 	    	var url = newwindow.location.href
-	    	console.error("Popup location is ", url, newwindow.location)
+	    	// console.error("Popup location is ", url, newwindow.location)
         resolve(url)
 			});
 
   		window.popupCompleted = function() {
 	    	var url = newwindow.location.href
-	    	console.error("Popup location is ", url, newwindow.location)
+	    	// console.error("Popup location is ", url, newwindow.location)
         resolve(url)
 			};
 
 			var newwindow = window.open(this.url, 'uwap-auth', 'height=600,width=800')
-      console.log("Newwindow is ", newwindow)
+      // console.log("Newwindow is ", newwindow)
       if (newwindow === null) {
         throw new Error("Error loading popup window")
       }
