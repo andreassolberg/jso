dbe5be28aea404f15bb378e8cfe99a7dfbb720d6
Andreas <andreas.solberg@uninett.no>
2018-03-21 11:39:59 +0100

    Comment out unused code in Popup loader

--- src/Loaders/Popup.js
+++ src/Loaders/Popup.js
@@ -10,23 +10,20 @@ export default class Popup extends BasicLoader {
 		*/
 		return new Promise((resolve, reject) => {
 
-			window.addEventListener("message", function(event) {
-				// console.log("Sent a message to event.origin " + event.origin + " and got the following in response:")
-				// console.log("<em>" + event.data + "</em>")
-
-	    	var url = newwindow.location.href
-	    	// console.error("Popup location is ", url, newwindow.location)
-        resolve(url)
-			})
+			// window.addEventListener("jso_message", function(event) {
+			// 	console.log("Sent a message to event.origin " + event.origin + " and got the following in response:")
+			// 	console.log("<em>", event.data, "</em>")
+	    // 	var url = newwindow.location.href
+	    // 	// console.error("Popup location is ", url, newwindow.location)
+      //   resolve(url)
+			// })
 
   		window.popupCompleted = function() {
 	    	var url = newwindow.location.href
-	    	// console.error("Popup location is ", url, newwindow.location)
         resolve(url)
 			}
 
 			var newwindow = window.open(this.url, 'jso-popup-auth', 'height=600,width=800')
-      // console.log("Newwindow is ", newwindow)
       if (newwindow === null) {
         throw new Error("Error loading popup window")
       }
