e4629eb21c88ad742fa6091474ffc6f136c23597
Andreas <andreas.solberg@uninett.no>
2018-04-03 12:57:47 +0200

    iframepassive work

--- src/Loaders/IFramePassive.js
+++ src/Loaders/IFramePassive.js
@@ -7,26 +7,62 @@ export default class HTTPRedirect extends BasicLoader {
 
     console.error("Constructor IFrame...")
 
-		var that = this;
-		this.timeout = 5000;
-		this.callback = null;
-		this.isCompleted = false;
+    console.error("URL IS: " +  url)
 
-		this.iframe = $('<iframe class="jso_messenger_iframe" style="display: none;" src="' + url + '"></iframe>')
-			.on('load', function() {
-				try {
-					var url = this.contentWindow.window.location.href;
-					that._completed(url);
+		this.timeout = 5000
+		this.callback = null
+		this.isCompleted = false
 
-				} catch(err) {
-					console.error("Security exception with iframe loading.", err);
-				}
+    // Create IE + others compatible event handler
+    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+    var eventer = window[eventMethod];
+    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
 
-			});
+    // Listen to message from child window
+    eventer(messageEvent,(e) => {
+      if (e.data === 'jso_iframe') {
+        let nurl = this.iframe.getAttribute('src')
+        console.error("URL", this.iframe)
+      }
+      console.log('parent received message!:  ',e.data);
+    },false)
+
+
+    this.iframe = document.createElement('iframe')
+    this.iframe.setAttribute('id', 'jso_passive_iframe')
+    this.iframe.setAttribute('src', url)
+
+    this.iframe.addEventListener('load', (e) => {
+      console.error(" IS Loaded! ", e)
+    })
+
+
+    // var el = document.getElementById('marker')
+    // el.parentNode.insertBefore(ifrm, el)
+    //
+		// this.iframe = $('<iframe class="jso_messenger_iframe" style="display: none;" src="' + url + '"></iframe>')
+		// 	.on('load', () => {
+		// 		try {
+		// 			var url = this.contentWindow.window.location.href;
+		// 			this._completed(url);
+    //
+		// 		} catch(err) {
+		// 			console.error("Security exception with iframe loading.", err);
+		// 		}
+    //
+		// 	});
 
 	}
 
 	execute() {
+
+
+    console.error(" ---- execute ----")
+    document.getElementsByTagName('body')[0].appendChild(this.iframe)
+
+    return Promise.resolve(null)
+
+
 		var that = this;
     console.error("Execute IFrame...")
 		var p = new Promise(function(resolve, reject) {
