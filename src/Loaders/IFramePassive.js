e20926d45735593cd981facb1501a9dcc5ff68ee
Andreas <andreas.solberg@uninett.no>
2018-04-04 12:00:16 +0200

    Improve iframepassive loader

--- src/Loaders/IFramePassive.js
+++ src/Loaders/IFramePassive.js
@@ -6,103 +6,84 @@ export default class HTTPRedirect extends BasicLoader {
 	constructor(url) {
 		super(url);
 
-    console.error("Constructor IFrame...")
-
-    console.error("URL IS: " +  url)
-
 		this.timeout = 5000
 		this.callback = null
 		this.isCompleted = false
+    this.id = 'jso_passive_iframe_' + utils.uuid()
 
     // Create IE + others compatible event handler
-    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-    var eventer = window[eventMethod];
-    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-
-    // Listen to message from child window
-    eventer(messageEvent,(e) => {
-      if (e.data === 'jso_iframe') {
-        let nurl = this.iframe.getAttribute('src')
-        console.error("URL", this.iframe)
-      }
-      console.log('parent received message!:  ',e.data);
-    },false)
-
+    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent"
+    var eventer = window[eventMethod]
+    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message"
 
     this.iframe = document.createElement('iframe')
-    this.iframe.setAttribute('id', 'jso_passive_iframe_' + utils.uuid())
+    this.iframe.setAttribute('id', this.id)
     this.iframe.setAttribute('src', url)
 
     this.iframe.addEventListener('load', (e) => {
-      console.error(" IS Loaded! ", e)
-      console.log(this.iframe.contentWindow.location.hash)
 
-      if (this.iframe.contentWindow.location.hash) {
-        let encodedHash = this.iframe.contentWindow.location.hash.substring(1)
-        let object = utils.parseQueryString(encodedHash)
-        console.log("OBJECT", object)
-      } else {
-        console.error("Failed to obtain response value from iframe")
+      let object = null
+      try {
+        if (this.iframe.contentWindow.location.hash) {
+          let encodedHash = this.iframe.contentWindow.location.hash.substring(1)
+          object = utils.parseQueryString(encodedHash)
+        } else if (this.iframe.contentWindow.location.search) {
+          let encodedHash = this.iframe.contentWindow.location.search.substring(1)
+          object = utils.parseQueryString(encodedHash)
+        }
+
+        if (object !== null) {
+          this._completed(object)
+        } else {
+          this._failed(new Error("Failed to obtain response value from iframe"))
+        }
+      } catch( err) {
+        // Most likely not able to access the content window because of same-origin policy.
+        //
+        // Ignore this error, as this is likely to happen during the SSO redirect loop, but the load
+        // event may be triggered multiple times, so it is not neccessary a problem that the first is not
+        // accessible.
       }
 
     })
 
-
-    // var el = document.getElementById('marker')
-    // el.parentNode.insertBefore(ifrm, el)
-    //
-		// this.iframe = $('<iframe class="jso_messenger_iframe" style="display: none;" src="' + url + '"></iframe>')
-		// 	.on('load', () => {
-		// 		try {
-		// 			var url = this.contentWindow.window.location.href;
-		// 			this._completed(url);
-    //
-		// 		} catch(err) {
-		// 			console.error("Security exception with iframe loading.", err);
-		// 		}
-    //
-		// 	});
-
 	}
 
 	execute() {
+		return new Promise((resolve, reject) => {
+			this.callback = resolve
+      this.errorCallback = reject
+      document.getElementsByTagName('body')[0].appendChild(this.iframe)
 
-
-    console.error(" ---- execute ----")
-    document.getElementsByTagName('body')[0].appendChild(this.iframe)
-
-    return Promise.resolve(null)
-
-
-		var that = this;
-    console.error("Execute IFrame...")
-		var p = new Promise(function(resolve, reject) {
-			that.callback = resolve;
-			$("body").prepend(that.iframe);
-			setTimeout(function() {
-				if (!that.isCompleted) {
-					that.isCompleted = true;
-					that._cleanup();
-					reject(new Error("Loading iframe timed out"));
-				}
-			}, that.timeout);
-		});
-		return p;
+			setTimeout(() => {
+				this._failed(new Error("Loading iframe timed out"))
+			}, this.timeout)
+		})
 	}
 
 
 	_cleanup() {
-		this.iframe.remove();
+    let element = document.getElementById(this.id)
+    element.parentNode.removeChild(element)
 	}
 
-	_completed(url) {
-		var that = this;
-		if (!that.isCompleted) {
-			if (that.callback && typeof that.callback === 'function') {
-				that.callback(url);
+  _failed(err) {
+    if (!this.isCompleted) {
+			if (this.errorCallback && typeof this.errorCallback === 'function') {
+				this.errorCallback(err)
 			}
-			this.isCompleted = true;
-			this._cleanup();
+			this.isCompleted = true
+			this._cleanup()
+		}
+  }
+
+	_completed(response) {
+		if (!this.isCompleted) {
+			if (this.callback && typeof this.callback === 'function') {
+				this.callback(response)
+			}
+			this.isCompleted = true
+			this._cleanup()
 		}
 	}
 }
