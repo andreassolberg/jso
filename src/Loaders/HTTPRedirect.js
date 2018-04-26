7aae793aea546f5be5211b67f9f532f4a1be893d
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2018-04-26 11:59:01 +0200

    Remove url logging, duplicate.

--- src/Loaders/HTTPRedirect.js
+++ src/Loaders/HTTPRedirect.js
@@ -4,7 +4,6 @@ import utils from '../utils'
 export default class HTTPRedirect extends BasicLoader {
 	execute() {
 		return new Promise((resolve, reject) => {
-      utils.log("HTTPRedirect sending user to url", this.url)
 			window.location = this.url
 			resolve(true)
 		})
